46ded6cf17a278545be5648a6b853783e3076767
Bethany Colon <bethany.colon28@gmail.com>
2023-11-02 21:40:04 -0400

    added spotify api into script.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,103 +1,40 @@
 console.log("connected test is good.");
 
-// find the form on the page
-var cityForm = document.querySelector("#city-form");
-// var cityForm = $('#city-form');
-var cityField = document.querySelector("#input-city");
-// var cityField = $('#input-city');
+const userInput = document.querySelector('input');
+const btn = document.querySelector('button');
 
-// event listener/handler/function
-function captureCity(event){
-  // stop refreshing
-  event.preventDefault();
-  console.log(event);
-  console.log(event.target);
+async function getMusic(query) {
+  const url = `https://spotify23.p.rapidapi.com/search/?q=${query}&type=albums&offset=0&limit=3&numberOfTopResults=5`;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f3fb0440b4msh682c2c923345172p1a9677jsn0e62cb010957',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	}
+};
 
-  firstFetch(cityField.value);
-  // jquery value capture
-  // firstFetch(cityField.val());
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(JSON.parse(result));
+} catch (error) {
+	console.error(error);
+}
 }
 
-
-function firstFetch(city){
-  var searchUrl = 'https://api.giphy.com/v1/gifs/search';
-  var apiKey = '?api_key=RANHjz2L1Drs7AUTUKTyWRWnP2iuiQpy'; //first_parameter
-  var additionOptions = '&limit=1&offset=0&rating=g&lang=en&bundle=messaging_non_clips'; // added by api explorer
-  var cityParam = "&q=" + city;
-  var url = searchUrl + apiKey + additionOptions + cityParam;
-  console.log(url);
-  fetch(url)// promise based
-  .then(function(response){
-    return response.json(); // parse the response data
-  })
-  .then(function(data) { // data should be an array or an object
-    console.log(data);
-    for(var i = 0; i < data.data.length; i++){
-      console.log(data.data[i].images.original.url);
-      var giphy = data.data[i].images.original.url;
-      // create
-      var imgEl = document.createElement('img');
-      // var imgEl = $('<img>');
-
-      // attr/text
-      imgEl.setAttribute('src', giphy);
-      // imgEl.attr('src', giphy);
-
-      // append
-      document.querySelector('#city-giphy').appendChild(imgEl);
-      // $('#city-giphy').append(imgEl);
-
-      // <section class="column is-half" id="city-giphy">
-        
-      // </section>
-
-      secondFetch(city);
-    }
-    
-    
-  })
-}
+getMusic("Star Wars");
 
 
+// fetch(url)// promise based
+//       .then(function (response) {
+//           return response.json(); // parse the response data
+//       })
+//       .then(function (data) { // data should be an array or an object
+//           console.log(data);
+//       })
 
-function secondFetch(city){
-  var searchUrl = 'https://api.giphy.com/v1/gifs/search';
-  var apiKey = '?api_key=RANHjz2L1Drs7AUTUKTyWRWnP2iuiQpy'; //first_parameter
-  var additionOptions = '&limit=1&offset=0&rating=g&lang=en&bundle=messaging_non_clips'; // added by api explorer
-  var cityParam = "&q=" + city + "+spongebob";
-  var url = searchUrl + apiKey + additionOptions + cityParam;
-  console.log(url);
-  fetch(url)// promise based
-  .then(function(response){
-    return response.json(); // parse the response data
-  })
-  .then(function(data) { // data should be an array or an object
-    console.log(data);
-    for(var i = 0; i < data.data.length; i++){
-      console.log(data.data[i].images.original.url);
-      var giphy = data.data[i].images.original.url;
-      // create
-      var imgEl = document.createElement('img');
-      // var imgEl = $('<img>');
-
-      // attr/text
-      imgEl.setAttribute('src', giphy);
-      // imgEl.attr('src', giphy);
-
-      // append
-      document.querySelector('#sponge-giphy').appendChild(imgEl);
-      // $('#sponge-giphy').append(imgEl);
-
-      // <section class="column is-half" id="sponge-giphy">
-        
-      // </section>
-
-    }
-    
-    
-  })
-}
+//  catch (error) {
+//   console.error(error);
+// }
 
 
-// addEventListener
-cityForm.addEventListener("submit", captureCity);
