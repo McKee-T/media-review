d9df9047596a43ed56cc758da73b2a08051baabe
Hannahphoto <hannahprice67@gmail.com>
2023-11-02 21:43:11 -0400

    api working for omdb

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,103 +1,31 @@
-console.log("connected test is good.");
+var searchButton = document.getElementById('searchButton');
+var input = document.getElementById('input');
 
-// find the form on the page
-var cityForm = document.querySelector("#city-form");
-// var cityForm = $('#city-form');
-var cityField = document.querySelector("#input-city");
-// var cityField = $('#input-city');
 
-// event listener/handler/function
-function captureCity(event){
-  // stop refreshing
+function omdbApi (){
   event.preventDefault();
-  console.log(event);
-  console.log(event.target);
-
-  firstFetch(cityField.value);
-  // jquery value capture
-  // firstFetch(cityField.val());
-}
-
-
-function firstFetch(city){
-  var searchUrl = 'https://api.giphy.com/v1/gifs/search';
-  var apiKey = '?api_key=RANHjz2L1Drs7AUTUKTyWRWnP2iuiQpy'; //first_parameter
-  var additionOptions = '&limit=1&offset=0&rating=g&lang=en&bundle=messaging_non_clips'; // added by api explorer
-  var cityParam = "&q=" + city;
-  var url = searchUrl + apiKey + additionOptions + cityParam;
-  console.log(url);
-  fetch(url)// promise based
+  var requestURL = "https://www.omdbapi.com/?t="+input.value+"&apikey=3c53385a&"
+  // var requestPoster = "https://img.omdbapi.com/?apikey=3c53385a&"
+  fetch(requestURL)
   .then(function(response){
-    return response.json(); // parse the response data
+    return response.json();
   })
-  .then(function(data) { // data should be an array or an object
+  .then(function(data){
     console.log(data);
-    for(var i = 0; i < data.data.length; i++){
-      console.log(data.data[i].images.original.url);
-      var giphy = data.data[i].images.original.url;
-      // create
-      var imgEl = document.createElement('img');
-      // var imgEl = $('<img>');
-
-      // attr/text
-      imgEl.setAttribute('src', giphy);
-      // imgEl.attr('src', giphy);
-
-      // append
-      document.querySelector('#city-giphy').appendChild(imgEl);
-      // $('#city-giphy').append(imgEl);
-
-      // <section class="column is-half" id="city-giphy">
-        
-      // </section>
-
-      secondFetch(city);
-    }
-    
-    
   })
-}
+  var title = data.Title;
+  var year = data.Year;
+  var actors = data.Actors;
+  var director = data.Director;
+  var writer = data.Writer;
+  var awards = data.Awards;
+  var listMovieInfo = document.createElement('li');
+  listMovieInfo.textContent = title +"<br>" + year+"<br>" + actors+"<br>" + director+"<br>" + writer+"<br>" + awards;
+  innerHTML.appendChild(listMovieInfo);
 
 
 
-function secondFetch(city){
-  var searchUrl = 'https://api.giphy.com/v1/gifs/search';
-  var apiKey = '?api_key=RANHjz2L1Drs7AUTUKTyWRWnP2iuiQpy'; //first_parameter
-  var additionOptions = '&limit=1&offset=0&rating=g&lang=en&bundle=messaging_non_clips'; // added by api explorer
-  var cityParam = "&q=" + city + "+spongebob";
-  var url = searchUrl + apiKey + additionOptions + cityParam;
-  console.log(url);
-  fetch(url)// promise based
-  .then(function(response){
-    return response.json(); // parse the response data
-  })
-  .then(function(data) { // data should be an array or an object
-    console.log(data);
-    for(var i = 0; i < data.data.length; i++){
-      console.log(data.data[i].images.original.url);
-      var giphy = data.data[i].images.original.url;
-      // create
-      var imgEl = document.createElement('img');
-      // var imgEl = $('<img>');
+};
 
-      // attr/text
-      imgEl.setAttribute('src', giphy);
-      // imgEl.attr('src', giphy);
-
-      // append
-      document.querySelector('#sponge-giphy').appendChild(imgEl);
-      // $('#sponge-giphy').append(imgEl);
-
-      // <section class="column is-half" id="sponge-giphy">
-        
-      // </section>
-
-    }
-    
-    
-  })
-}
-
-
-// addEventListener
-cityForm.addEventListener("submit", captureCity);
+searchButton.addEventListener("click", omdbApi);
+// input.addEventListener("input");
